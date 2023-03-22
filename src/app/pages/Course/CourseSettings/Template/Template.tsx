7d1409c1a4c6d6ad1dd125147fa8bf85c6f76d0f
Ali Hassan <alihassanyussef@gmail.com>
2023-03-22 03:08:26 +0200

    binding value with text-fields

--- src/app/pages/Course/CourseSettings/Template/Template.tsx
+++ src/app/pages/Course/CourseSettings/Template/Template.tsx
@@ -1,60 +1,76 @@
-import { Divider, Grid, IconButton, InputAdornment, List, ListItem, ListItemIcon, ListItemText, TextField, Typography } from '@mui/material'
-import { Box } from '@mui/system'
-import React, { useState } from 'react'
-import EditIcon from '@mui/icons-material/Edit';
-import { SquareRounded } from '@mui/icons-material';
-
+import {
+  Divider,
+  Grid,
+  IconButton,
+  InputAdornment,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { Box } from "@mui/system";
+import React, { useState } from "react";
+import EditIcon from "@mui/icons-material/Edit";
+import { SquareRounded } from "@mui/icons-material";
 
 export default function Template() {
-  const [questionType, updateTypePerc] = useState([{ name: "MCQ", percent: 50 }, { name: "T/F", percent: 50 }])
+  const [questionType, updateTypePerc] = useState([
+    { name: "MCQ", percent: 50 },
+    { name: "T/F", percent: 50 },
+  ]);
   const [percType, editPercType] = useState(questionType.map((value) => false));
 
-  const [difficulty, updateDiffPerc] = useState([{ level: "Easy", percent: 50 }, { level: "Medium", percent: 30 }, { level: "Hard", percent: 20 }])
+  const [difficulty, updateDiffPerc] = useState([
+    { level: "Easy", percent: 50 },
+    { level: "Medium", percent: 30 },
+    { level: "Hard", percent: 20 },
+  ]);
   const [percDiff, editPercDiff] = useState([false, false, false]);
 
-  const colors = ["#3FC164","#FFAC4B","#FF4B4B"]
+  const colors = ["#3FC164", "#FFAC4B", "#FF4B4B"];
 
   const openEditType = (index: number) => {
-    let types = [...percType]
-    types[index] = !types[index]
-    editPercType(types)
-  }
+    let types = [...percType];
+    types[index] = !types[index];
+    editPercType(types);
+  };
 
   const openEditDiff = (index: number) => {
-    let diff = [...percDiff]
-    diff[index] = !diff[index]
-    editPercDiff(diff)
-  }
+    let diff = [...percDiff];
+    diff[index] = !diff[index];
+    editPercDiff(diff);
+  };
 
   const handleTypeChange = (event: any, index: number) => {
-    if (event.key == 'Enter') {
-      let types = [...questionType]
+    if (event.key == "Enter") {
+      let types = [...questionType];
       types[index].percent = event.target.value;
-      updateTypePerc(types)
-      openEditType(index)
+      updateTypePerc(types);
+      openEditType(index);
     }
-  }
+  };
 
   const handleDiffChange = (event: any, index: number) => {
-    if (event.key == 'Enter') {
-      let diff = [...difficulty]
+    if (event.key == "Enter") {
+      let diff = [...difficulty];
       diff[index].percent = event.target.value;
-      updateDiffPerc(diff)
-      openEditDiff(index)
+      updateDiffPerc(diff);
+      openEditDiff(index);
     }
-  }
-
-
+  };
 
   return (
     <Box>
-      <Box sx={{
-        fontSize: "2rem",
-        fontWeight: "medium",
-        fontFamily: "montserrat",
-      }}>
+      <Typography
+        sx={{
+          fontSize: "2rem",
+          fontWeight: "medium",
+        }}
+      >
         Exam Templete
-      </Box>
+      </Typography>
       <Box
         sx={{
           marginTop: "10px",
@@ -62,50 +78,75 @@ export default function Template() {
           bgcolor: "White",
           borderRadius: "15px",
           paddingBottom: "8px",
-        }}>
+        }}
+      >
         <Typography
           sx={{
             marginLeft: "10px",
             paddingTop: "8px",
             fontSize: "23px",
-          }}>
+          }}
+        >
           Question Types
         </Typography>
-        <List
-          disablePadding>
+        <List disablePadding>
           {questionType.map((value, index) => {
             return (
               <Box>
                 <Divider sx={{ color: "#D9D9D9" }}></Divider>
-                <ListItem 
-                sx={{
-                  paddingBottom: "5px",
-                  paddingTop: "5px"
-                }}
-                secondaryAction={
-                  <IconButton onClick={() => openEditType(index)}>
-                    <EditIcon />
-                  </IconButton>
-                }>
-                  <Grid container
-                  sx={{marginLeft: "4px"}}>
+                <ListItem
+                  sx={{
+                    paddingBottom: "5px",
+                    paddingTop: "5px",
+                  }}
+                  secondaryAction={
+                    <IconButton onClick={() => openEditType(index)}>
+                      <EditIcon />
+                    </IconButton>
+                  }
+                >
+                  <Grid container sx={{ marginLeft: "4px" }}>
                     <Grid item xs={3}>
-                      <ListItemText primary={value.name} 
-                      sx={{ color: "#6B6767", width: '100%', 
-                      fontSize: "15px", textTransform: 'none' 
-                      }}></ListItemText>
+                      <ListItemText
+                        primary={value.name}
+                        sx={{
+                          color: "#6B6767",
+                          width: "100%",
+                          fontSize: "15px",
+                          textTransform: "none",
+                        }}
+                      />
                     </Grid>
                     <Grid item xs={3}>
-                      {percType[index] ? <TextField
-                        onKeyDown={(event) => handleTypeChange(event, index)}
-                        placeholder='%'
-                        InputProps={{sx:{height: "30px", "& input":{textAlign:"right"}}}}
-                        sx={{
-                          bgcolor: "#F5F5F5",
-                          borderColor: "#D9D9D9",
-                          width: "100%",
-                          marginTop:"2px"
-                        }}></TextField> : <ListItemText primary={value.percent} sx={{ color: "#6B6767", width: '100%',fontSize: "15px", textTransform: 'none' }}></ListItemText>}
+                      {percType[index] ? (
+                        <TextField
+                          onKeyDown={(event) => handleTypeChange(event, index)}
+                          placeholder="%"
+                          value={value.percent}
+                          InputProps={{
+                            sx: {
+                              height: "30px",
+                              "& input": { textAlign: "right" },
+                            },
+                          }}
+                          sx={{
+                            bgcolor: "#F5F5F5",
+                            borderColor: "#D9D9D9",
+                            width: "100%",
+                            marginTop: "2px",
+                          }}
+                        />
+                      ) : (
+                        <ListItemText
+                          primary={value.percent}
+                          sx={{
+                            color: "#6B6767",
+                            width: "100%",
+                            fontSize: "15px",
+                            textTransform: "none",
+                          }}
+                        ></ListItemText>
+                      )}
                     </Grid>
                   </Grid>
                 </ListItem>
@@ -120,58 +161,84 @@ export default function Template() {
           marginLeft: "20px",
           bgcolor: "White",
           borderRadius: "15px",
-          paddingBottom:"8px"
-        }}>
+          paddingBottom: "8px",
+        }}
+      >
         <Typography
           sx={{
             marginLeft: "10px",
             paddingTop: "8px",
             fontSize: "23px",
-          }}>
+          }}
+        >
           Difficulty Levels
         </Typography>
-        <List
-          disablePadding>
+        <List disablePadding>
           {difficulty.map((value, index) => {
             return (
               <Box>
                 <Divider sx={{ color: "#D9D9D9" }}></Divider>
-                <ListItem 
-                sx={{
-                  paddingBottom: "5px",
-                  paddingTop: "5px"
-                }}
-                secondaryAction={
-                  <IconButton onClick={() => openEditDiff(index)}>
-                    <EditIcon />
-                  </IconButton>
-                }>
-                  <ListItemIcon sx={{minWidth: "30px", marginLeft:"2px"}}>
+                <ListItem
+                  sx={{
+                    paddingBottom: "5px",
+                    paddingTop: "5px",
+                  }}
+                  secondaryAction={
+                    <IconButton onClick={() => openEditDiff(index)}>
+                      <EditIcon />
+                    </IconButton>
+                  }
+                >
+                  <ListItemIcon sx={{ minWidth: "30px", marginLeft: "2px" }}>
                     <SquareRounded
-                    fontSize='medium'
-                    sx={{
-                      color: colors[index],
-                    }}></SquareRounded>
+                      fontSize="medium"
+                      sx={{
+                        color: colors[index],
+                      }}
+                    ></SquareRounded>
                   </ListItemIcon>
-                  <Grid container
-                  sx={{marginLeft: "2px",}}>
+                  <Grid container sx={{ marginLeft: "2px" }}>
                     <Grid item xs={3}>
-                      <ListItemText primary={value.level} 
-                      sx={{ color: "#6B6767", width: '100%', 
-                      fontSize: "15px", textTransform: 'none' 
-                      }}></ListItemText>
+                      <ListItemText
+                        primary={value.level}
+                        sx={{
+                          color: "#6B6767",
+                          width: "100%",
+                          fontSize: "15px",
+                          textTransform: "none",
+                        }}
+                      ></ListItemText>
                     </Grid>
                     <Grid item xs={3}>
-                      {percDiff[index] ? <TextField
-                        onKeyDown={(event) => handleDiffChange(event, index)}
-                        placeholder='%'
-                        InputProps={{sx:{height: "30px", "& input":{textAlign:"right"}}}}
-                        sx={{
-                          bgcolor: "#F5F5F5",
-                          borderColor: "#D9D9D9",
-                          width: "100%",
-                          marginTop:"2px"
-                        }}></TextField> : <ListItemText primary={value.percent} sx={{ color: "#6B6767", width: '100%',fontSize: "15px", textTransform: 'none' }}></ListItemText>}
+                      {percDiff[index] ? (
+                        <TextField
+                          onKeyDown={(event) => handleDiffChange(event, index)}
+                          placeholder="%"
+                          value={value.percent}
+                          InputProps={{
+                            sx: {
+                              height: "30px",
+                              "& input": { textAlign: "right" },
+                            },
+                          }}
+                          sx={{
+                            bgcolor: "#F5F5F5",
+                            borderColor: "#D9D9D9",
+                            width: "100%",
+                            marginTop: "2px",
+                          }}
+                        ></TextField>
+                      ) : (
+                        <ListItemText
+                          primary={value.percent}
+                          sx={{
+                            color: "#6B6767",
+                            width: "100%",
+                            fontSize: "15px",
+                            textTransform: "none",
+                          }}
+                        ></ListItemText>
+                      )}
                     </Grid>
                   </Grid>
                 </ListItem>
@@ -181,5 +248,5 @@ export default function Template() {
         </List>
       </Box>
     </Box>
-  )
+  );
 }
