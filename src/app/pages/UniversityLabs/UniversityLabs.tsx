ba36699ef7aa1c25ea14b02594d2c18247d7df38
MennaTallah Samir <manna.samir201@gmail.com>
2023-06-23 08:25:18 -0700

    Add Lab UI done

--- src/app/pages/UniversityLabs/UniversityLabs.tsx
+++ src/app/pages/UniversityLabs/UniversityLabs.tsx
@@ -1,9 +1,12 @@
-import { Box, Button, Card, Divider, IconButton, Menu, MenuItem, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, styled, tableCellClasses } from '@mui/material'
-import React, { useEffect } from 'react';
+import { Box, Button, Card, Divider, IconButton, Menu, MenuItem, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TextField, styled, tableCellClasses } from '@mui/material'
+import React, { useEffect, useState } from 'react';
 import { ILab } from '../../types/Lab';
 import { mockLabs } from '../../services/APIs/mockData/MockData';
 import MoreVertIcon from "@mui/icons-material/MoreVert";
+import CheckOutlinedIcon from '@mui/icons-material/CheckOutlined';
+import ClearIcon from '@mui/icons-material/Clear';
 import theme from '../../../assets/theme';
+import { LensBlurSharp } from '@mui/icons-material';
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
   [`&.${tableCellClasses.head}`]: {
@@ -17,8 +20,16 @@ const StyledTableCell = styled(TableCell)(({ theme }) => ({
 
 export default function UniversityLabs() {
   const tableHeader = ["Lab Name", "Capacity", ""];
-  const labs: ILab[] = mockLabs;
+  const [labs, setLabs] = useState(mockLabs);
 
+  useEffect(() => {
+    // getLabsListApi().then((res) => {
+    //   console.log(res);
+    // });
+
+  }, []);
+
+  const [addLab, openAddLab] = useState(false);
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
   const open = Boolean(anchorEl);
   const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
@@ -30,16 +41,47 @@ export default function UniversityLabs() {
     event.stopPropagation();
   };
 
+  const [LabName, setLabName] = useState("");
+  const [LabCapacity, setLabCapacity] = useState<string | null>(null);
+  const [errorMsg, setErrorMsg] = useState<string | null>(null);
+
+  const handleCloseAdd = () => {
+    setLabName("");
+    setLabCapacity(null);
+    openAddLab(false);
+  }
+
+  const validateChange = () => {
+    let error = false;
+    if (LabName === "") {
+      setErrorMsg("Empty Input!");
+      error = true;
+    } else if (LabCapacity == null) {
+      setErrorMsg("Empty Input!");
+      error = true;
+    } else {
+      labs.forEach((lab) => {
+        if (lab.name == LabName) {
+          setErrorMsg("Topic Name Already Exists!");
+          error = true;
+        }
+      });
+    }
+    if (!error) {
+      var newLab: ILab = {
+        id: labs.at(labs.length - 1)?.id,
+        name: LabName,
+        capacity: Number(LabCapacity),
+      }
+      setLabs([...labs, newLab]);
+      setErrorMsg(null);
+      handleCloseAdd();
+    }
+  };
+
   const handleEdit = () => { }
 
   const handleDelete = () => { }
-
-  useEffect(() => {
-    // getLabsListApi().then((res) => {
-    //   console.log(res);
-    // });
-
-  }, []);
   return (
     <Box sx={{ px: 15, py: 5 }}>
       <Box
@@ -59,7 +101,7 @@ export default function UniversityLabs() {
           bgcolor: "White",
           borderRadius: "10px",
         }}>
-        <TableContainer sx={{ borderRadius: "10px"}}>
+        <TableContainer sx={{ borderRadius: "10px" }}>
           <Table>
             <TableHead sx={{ color: "#F5F5F5" }}>
               <TableRow>
@@ -113,15 +155,68 @@ export default function UniversityLabs() {
                   </StyledTableCell>
                 </TableRow>
               )}
+              {addLab ?
+                <TableRow>
+                  <StyledTableCell align="center">
+                    <TextField
+                    onChange={(event) => setLabName(event.target.value)}
+                    value={LabName}
+                      placeholder="Enter new lab name"
+                      error={errorMsg ? true : false}
+                      helperText={errorMsg}
+                      InputProps={{ sx: { height: "32px" } }}
+                      sx={{
+                        bgcolor: "#F5F5F5",
+                        borderColor: "#D9D9D9",
+                        "& .MuiFormLabel-root": {
+                          fontSize: "14px",
+                          fontWeight: "medium",
+                        },
+                      }}></TextField>
+                  </StyledTableCell>
+                  <StyledTableCell align="center">
+                    <TextField
+                    onChange={(event) => setLabCapacity(event.target.value)}
+                    value={LabCapacity}
+                      placeholder="Enter lab capacity"
+                      error={errorMsg ? true : false}
+                      helperText={errorMsg}
+                      InputProps={{ sx: { height: "32px" } }}
+                      sx={{
+                        bgcolor: "#F5F5F5",
+                        borderColor: "#D9D9D9",
+                        "& .MuiFormLabel-root": {
+                          fontSize: "14px",
+                          fontWeight: "medium",
+                        },
+                      }}></TextField>
+                  </StyledTableCell>
+                  <StyledTableCell align="right">
+                    <IconButton
+                      onClick={handleCloseAdd}>
+                      <ClearIcon
+                        sx={{
+                          color: "#f44336",
+                        }}
+                      />
+                    </IconButton>
+                    <IconButton
+                      onClick={validateChange}>
+                      <CheckOutlinedIcon
+                        color="primary" />
+                    </IconButton>
+                  </StyledTableCell>
+                </TableRow>
+                : <></>}
             </TableBody>
           </Table>
         </TableContainer>
         <Box display="flex"
           justifyContent="flex-end"
         >
-          <Button
+          {!addLab ? <Button
             variant="outlined"
-            onClick={() => null}
+            onClick={() => openAddLab(true)}
             sx={{
               color: "#1B84BF",
               backgroundColor: theme.palette.white.main,
@@ -137,7 +232,7 @@ export default function UniversityLabs() {
             }}
           >
             Add Lab
-          </Button>
+          </Button> : <></>}
         </Box>
       </Card>
     </Box>
