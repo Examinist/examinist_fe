28342a60eef092d1c11073e3bc37a496e198af3a
Noha Ahmed <noha.morsy2000@gmail.com>
2023-06-26 03:26:53 +0300

    WIP

--- src/app/pages/Student/Exam/Questions/Question/Answers/MCQMultipleAnswers.tsx
+++ src/app/pages/Student/Exam/Questions/Question/Answers/MCQMultipleAnswers.tsx
@@ -1,8 +1,48 @@
-import React from 'react'
-import IAnswerProbs from './AnswerProbs'
+import React from "react";
+import IAnswerProbs from "./AnswerProbs";
+import {
+  FormControl,
+  FormLabel,
+  FormGroup,
+  FormControlLabel,
+  Checkbox,
+  FormHelperText,
+} from "@mui/material";
 
-export default function MCQMultipleAnswers({answer, onUpdate}: IAnswerProbs) {
+export default function MCQMultipleAnswers({ answer, onUpdate }: IAnswerProbs) {
+  const [selectedChoices, setSelectedChoices] = React.useState<string[]>(
+    answer.answers
+  );
   return (
-    <div>MCQMultipleAnswers</div>
-  )
+    <FormControl sx={{ ml: 2 }} component="fieldset" variant="standard">
+      <FormLabel component="legend">Select all right answers:</FormLabel>
+      <FormGroup>
+        {answer.question?.choices.map((choice) => (
+          <FormControlLabel
+            key={choice.choice}
+            control={
+              <Checkbox
+                checked={selectedChoices.indexOf(choice.choice) !== -1}
+                onChange={(e) => {
+                  const newValue = choice.choice;
+                  let newSelectedChoices;
+                  if (selectedChoices.indexOf(choice.choice) !== -1) {
+                    newSelectedChoices = selectedChoices.filter(
+                      (choice) => choice !== newValue
+                    );
+                  } else {
+                    newSelectedChoices = [...selectedChoices, newValue];
+                  }
+                  setSelectedChoices(newSelectedChoices);
+                  onUpdate(newSelectedChoices);
+                }}
+                name={choice.choice}
+              />
+            }
+            label={choice.choice}
+          />
+        ))}
+      </FormGroup>
+    </FormControl>
+  );
 }
--- src/app/pages/Student/Exam/Questions/Question/Answers/MCQSingleAnswer.tsx
+++ src/app/pages/Student/Exam/Questions/Question/Answers/MCQSingleAnswer.tsx
@@ -1,6 +1,44 @@
-import React from 'react'
-import IAnswerProbs from './AnswerProbs';
+import React from "react";
+import IAnswerProbs from "./AnswerProbs";
+import {
+  FormControl,
+  RadioGroup,
+  FormControlLabel,
+  Radio,
+  FormLabel,
+} from "@mui/material";
 
 export default function MCQSingleAnswer({ answer, onUpdate }: IAnswerProbs) {
-  return <div>MCQSingleAnswer</div>;
+  const [value, setValue] = React.useState(
+    answer.answers.length > 0 ? answer.answers[0] : ""
+  );
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newValue = (event.target as HTMLInputElement).value;
+    setValue(newValue);
+    if (answer.answers.length === 0 || newValue !== answer.answers[0]) {
+      onUpdate([newValue]);
+    }
+  };
+
+  return (
+    <FormControl sx={{ ml: 2 }}>
+      <FormLabel component="legend">Select single answer:</FormLabel>
+      <RadioGroup
+        aria-labelledby="demo-controlled-radio-buttons-group"
+        name="controlled-radio-buttons-group"
+        value={value}
+        onChange={handleChange}
+      >
+        {answer.question?.choices.map((choice) => (
+          <FormControlLabel
+            key={choice.choice}
+            value={choice.choice}
+            control={<Radio />}
+            label={choice.choice}
+          />
+        ))}
+      </RadioGroup>
+    </FormControl>
+  );
 }
