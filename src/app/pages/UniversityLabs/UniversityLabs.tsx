2190d02ae17415d882516602d0b46bb1650a2452
Noha Ahmed <noha.morsy2000@gmail.com>
2023-06-27 18:38:24 +0300

    labs integeration in progress.

--- src/app/pages/UniversityLabs/UniversityLabs.tsx
+++ src/app/pages/UniversityLabs/UniversityLabs.tsx
@@ -1,12 +1,39 @@
-import { Box, Button, Card, Divider, IconButton, Menu, MenuItem, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TextField, styled, tableCellClasses } from '@mui/material'
-import React, { useEffect, useState } from 'react';
-import { ILab } from '../../types/Lab';
-import { mockLabs } from '../../services/APIs/mockData/MockData';
+import {
+  Box,
+  Button,
+  Card,
+  Divider,
+  IconButton,
+  Menu,
+  MenuItem,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  TextField,
+  styled,
+  tableCellClasses,
+} from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { ILab } from "../../types/Lab";
+import { mockLabs } from "../../services/APIs/mockData/MockData";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
-import CheckOutlinedIcon from '@mui/icons-material/CheckOutlined';
-import ClearIcon from '@mui/icons-material/Clear';
-import theme from '../../../assets/theme';
-import { LensBlurSharp } from '@mui/icons-material';
+import CheckOutlinedIcon from "@mui/icons-material/CheckOutlined";
+import ClearIcon from "@mui/icons-material/Clear";
+import theme from "../../../assets/theme";
+import { LensBlurSharp } from "@mui/icons-material";
+import {
+  ILabResponse,
+  ILabsListResponse,
+  addLabApi,
+  deleteLabApi,
+  getLabsListApi,
+} from "../../services/APIs/LabsAPIs";
+import useAlert from "../../hooks/useAlert";
+import { IErrorResponse } from "../../services/Response";
+import CustomCircularProgress from "../../components/CustomCircularProgress";
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
   [`&.${tableCellClasses.head}`]: {
@@ -20,13 +47,31 @@ const StyledTableCell = styled(TableCell)(({ theme }) => ({
 
 export default function UniversityLabs() {
   const tableHeader = ["Lab Name", "Capacity", ""];
-  const [labs, setLabs] = useState(mockLabs);
+  const [labs, setLabs] = useState<ILab[]>([]);
+  const { setAlertState } = useAlert();
+  const [isloading, setIsLoading] = useState<boolean>(false);
+
+  const loadLabs = () => {
+    setIsLoading(true);
+    getLabsListApi()
+      .then(({ data }: ILabsListResponse) => {
+        console.log(data);
+        setLabs(data.labs);
+      })
+      .catch(({ response: { status, statusText, data } }: IErrorResponse) => {
+        setAlertState({
+          open: true,
+          message: data?.message! || statusText,
+          severity: "error",
+        });
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  };
 
   useEffect(() => {
-    // getLabsListApi().then((res) => {
-    //   console.log(res);
-    // });
-
+    loadLabs();
   }, []);
 
   const [addLab, openAddLab] = useState(false);
@@ -49,7 +94,7 @@ export default function UniversityLabs() {
     setLabName("");
     setLabCapacity(null);
     openAddLab(false);
-  }
+  };
 
   const validateChange = () => {
     let error = false;
@@ -67,174 +112,225 @@ export default function UniversityLabs() {
         }
       });
     }
+    setErrorMsg(null);
     if (!error) {
       var newLab: ILab = {
-        id: labs.at(labs.length - 1)?.id,
         name: LabName,
         capacity: Number(LabCapacity),
-      }
-      setLabs([...labs, newLab]);
-      setErrorMsg(null);
-      handleCloseAdd();
+      };
+      addLabApi(newLab)
+        .then(() => {
+          loadLabs();
+          handleCloseAdd();
+        })
+        .catch(({ response: { status, statusText, data } }: IErrorResponse) => {
+          setAlertState({
+            open: true,
+            message: data?.message! || statusText,
+            severity: "error",
+          });
+        });
     }
   };
 
-  const handleEdit = () => { }
+  const handleEdit = () => {};
 
-  const handleDelete = () => { }
+  const handleDelete = (
+    event: React.MouseEvent<HTMLButtonElement>,
+    lab: ILab
+  ) => {
+    console.log("in delete", lab);
+    // if (lab.id) {
+    //   deleteLabApi(lab.id)
+    //     .then(() => {
+    //       loadLabs();
+    //     })
+    //     .catch(({ response: { status, statusText, data } }: IErrorResponse) => {
+    //       setAlertState({
+    //         open: true,
+    //         message: data?.message! || statusText,
+    //         severity: "error",
+    //       });
+    //     })
+    //     .finally(()=>{
+    //       setAnchorEl(null);
+    //       event.stopPropagation();
+    //     });
+    // }
+  };
   return (
-    <Box sx={{ px: 15, py: 5 }}>
-      <Box
-        sx={{
-          fontSize: "2rem",
-          fontWeight: "medium",
-          fontFamily: "montserrat",
-          paddingBottom: "10px"
-        }}
-      >
-        University Labs
-      </Box>
-      <Card
-        sx={{
-          paddingX: "25px",
-          paddingY: "35px",
-          bgcolor: "White",
-          borderRadius: "10px",
-        }}>
-        <TableContainer sx={{ borderRadius: "10px" }}>
-          <Table>
-            <TableHead sx={{ color: "#F5F5F5" }}>
-              <TableRow>
-                {tableHeader.map((value, index) => (
-                  <StyledTableCell
-                    sx={{ fontSize: 16 }}
-                    align="center"
-                  >{value}</StyledTableCell>
-                ))
-                }
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {labs.map((value) =>
-                <TableRow>
-                  <StyledTableCell align="center">{value.name}</StyledTableCell>
-                  <StyledTableCell align="center">{value.capacity}</StyledTableCell>
-                  <StyledTableCell align='right'>
-                    <div>
-                      <IconButton
-                        aria-label="more"
-                        id="long-button"
-                        aria-controls={open ? "long-menu" : undefined}
-                        aria-expanded={open ? "true" : undefined}
-                        aria-haspopup="true"
-                        onClick={handleClick}>
-                        <MoreVertIcon />
-                      </IconButton>
-                      <Menu
-                        id="basic-menu"
-                        anchorEl={anchorEl}
-                        open={open}
-                        onClose={handleClose}
-                        MenuListProps={{
-                          "aria-labelledby": "basic-button",
-                        }}
-                      >
-                        <MenuItem
-                          sx={{ minWidth: "150px" }}
-                          onClick={handleEdit}
-                        >Edit
-                        </MenuItem>
-                        <Divider />
-                        <MenuItem
-                          sx={{ minWidth: "150px" }}
-                          onClick={handleDelete}
-                        >Delete
-                        </MenuItem>
-                      </Menu>
-                    </div>
-                  </StyledTableCell>
-                </TableRow>
-              )}
-              {addLab ?
-                <TableRow>
-                  <StyledTableCell align="center">
-                    <TextField
-                    onChange={(event) => setLabName(event.target.value)}
-                    value={LabName}
-                      placeholder="Enter new lab name"
-                      error={errorMsg ? true : false}
-                      helperText={errorMsg}
-                      InputProps={{ sx: { height: "32px" } }}
-                      sx={{
-                        bgcolor: "#F5F5F5",
-                        borderColor: "#D9D9D9",
-                        "& .MuiFormLabel-root": {
-                          fontSize: "14px",
-                          fontWeight: "medium",
-                        },
-                      }}></TextField>
-                  </StyledTableCell>
-                  <StyledTableCell align="center">
-                    <TextField
-                    onChange={(event) => setLabCapacity(event.target.value)}
-                    value={LabCapacity}
-                      placeholder="Enter lab capacity"
-                      error={errorMsg ? true : false}
-                      helperText={errorMsg}
-                      InputProps={{ sx: { height: "32px" } }}
-                      sx={{
-                        bgcolor: "#F5F5F5",
-                        borderColor: "#D9D9D9",
-                        "& .MuiFormLabel-root": {
-                          fontSize: "14px",
-                          fontWeight: "medium",
-                        },
-                      }}></TextField>
-                  </StyledTableCell>
-                  <StyledTableCell align="right">
-                    <IconButton
-                      onClick={handleCloseAdd}>
-                      <ClearIcon
-                        sx={{
-                          color: "#f44336",
-                        }}
-                      />
-                    </IconButton>
-                    <IconButton
-                      onClick={validateChange}>
-                      <CheckOutlinedIcon
-                        color="primary" />
-                    </IconButton>
-                  </StyledTableCell>
-                </TableRow>
-                : <></>}
-            </TableBody>
-          </Table>
-        </TableContainer>
-        <Box display="flex"
-          justifyContent="flex-end"
-        >
-          {!addLab ? <Button
-            variant="outlined"
-            onClick={() => openAddLab(true)}
+    <>
+      {isloading ? (
+        <CustomCircularProgress />
+      ) : (
+        <Box sx={{ px: 15, py: 5 }}>
+          <Box
             sx={{
-              color: "#1B84BF",
-              backgroundColor: theme.palette.white.main,
-              width: "15%",
-              marginTop: "7px",
-              border: 1,
-              fontSize: "15px",
-              fontWeight: "bold",
-              textTransform: "none",
-              borderRadius: "15px",
-              py: 1.5,
-              height: "40px"
+              fontSize: "2rem",
+              fontWeight: "medium",
+              fontFamily: "montserrat",
+              paddingBottom: "10px",
             }}
           >
-            Add Lab
-          </Button> : <></>}
+            University Labs
+          </Box>
+          <Card
+            sx={{
+              paddingX: "25px",
+              paddingY: "35px",
+              bgcolor: "White",
+              borderRadius: "10px",
+            }}
+          >
+            <TableContainer sx={{ borderRadius: "10px" }}>
+              <Table>
+                <TableHead sx={{ color: "#F5F5F5" }}>
+                  <TableRow>
+                    {tableHeader.map((value, index) => (
+                      <StyledTableCell sx={{ fontSize: 16 }} align="center">
+                        {value}
+                      </StyledTableCell>
+                    ))}
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {labs.map((lab, index) => (
+                    <TableRow key={lab.id!}>
+                      <StyledTableCell align="center">
+                        {lab.name}
+                      </StyledTableCell>
+                      <StyledTableCell align="center">
+                        {lab.capacity}
+                      </StyledTableCell>
+                      <StyledTableCell align="right">
+                        <div>
+                          <IconButton
+                            aria-label="more"
+                            id="long-button"
+                            aria-controls={open ? "long-menu" : undefined}
+                            aria-expanded={open ? "true" : undefined}
+                            aria-haspopup="true"
+                            onClick={handleClick}
+                          >
+                            <MoreVertIcon />
+                          </IconButton>
+                          <Menu
+                            id="basic-menu"
+                            anchorEl={anchorEl}
+                            open={open}
+                            onClose={handleClose}
+                            MenuListProps={{
+                              "aria-labelledby": "basic-button",
+                            }}
+                          >
+                            <MenuItem
+                              sx={{ minWidth: "150px" }}
+                              onClick={handleEdit}
+                            >
+                              Edit
+                            </MenuItem>
+                            <Divider />
+                            <MenuItem
+                              sx={{ minWidth: "150px" }}
+                              onClick={(event: any) => {
+                                console.log(index, lab)
+                                handleDelete(event, lab);
+                              }}
+                            >
+                              Delete
+                            </MenuItem>
+                          </Menu>
+                        </div>
+                      </StyledTableCell>
+                    </TableRow>
+                  ))}
+                  {addLab ? (
+                    <TableRow>
+                      <StyledTableCell align="center">
+                        <TextField
+                          onChange={(event) => setLabName(event.target.value)}
+                          value={LabName}
+                          placeholder="Enter new lab name"
+                          error={errorMsg ? true : false}
+                          helperText={errorMsg}
+                          InputProps={{ sx: { height: "32px" } }}
+                          sx={{
+                            bgcolor: "#F5F5F5",
+                            borderColor: "#D9D9D9",
+                            "& .MuiFormLabel-root": {
+                              fontSize: "14px",
+                              fontWeight: "medium",
+                            },
+                          }}
+                        ></TextField>
+                      </StyledTableCell>
+                      <StyledTableCell align="center">
+                        <TextField
+                          onChange={(event) =>
+                            setLabCapacity(event.target.value)
+                          }
+                          value={LabCapacity}
+                          placeholder="Enter lab capacity"
+                          error={errorMsg ? true : false}
+                          helperText={errorMsg}
+                          InputProps={{ sx: { height: "32px" } }}
+                          sx={{
+                            bgcolor: "#F5F5F5",
+                            borderColor: "#D9D9D9",
+                            "& .MuiFormLabel-root": {
+                              fontSize: "14px",
+                              fontWeight: "medium",
+                            },
+                          }}
+                        ></TextField>
+                      </StyledTableCell>
+                      <StyledTableCell align="right">
+                        <IconButton onClick={handleCloseAdd}>
+                          <ClearIcon
+                            sx={{
+                              color: "#f44336",
+                            }}
+                          />
+                        </IconButton>
+                        <IconButton onClick={validateChange}>
+                          <CheckOutlinedIcon color="primary" />
+                        </IconButton>
+                      </StyledTableCell>
+                    </TableRow>
+                  ) : (
+                    <></>
+                  )}
+                </TableBody>
+              </Table>
+            </TableContainer>
+            <Box display="flex" justifyContent="flex-end">
+              {!addLab ? (
+                <Button
+                  variant="outlined"
+                  onClick={() => openAddLab(true)}
+                  sx={{
+                    color: "#1B84BF",
+                    backgroundColor: theme.palette.white.main,
+                    width: "15%",
+                    marginTop: "7px",
+                    border: 1,
+                    fontSize: "15px",
+                    fontWeight: "bold",
+                    textTransform: "none",
+                    borderRadius: "15px",
+                    py: 1.5,
+                    height: "40px",
+                  }}
+                >
+                  Add Lab
+                </Button>
+              ) : (
+                <></>
+              )}
+            </Box>
+          </Card>
         </Box>
-      </Card>
-    </Box>
-  )
+      )}
+    </>
+  );
 }
