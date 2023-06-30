6d74ab3af11a14db3c66993c550328464ebffb42
MennaTallah Samir <manna.samir201@gmail.com>
2023-06-30 11:18:48 -0700

    Control view 1 done

--- src/app/pages/ExamsLabs/ExamLabs.tsx
+++ src/app/pages/ExamsLabs/ExamLabs.tsx
@@ -1,14 +1,16 @@
-import { Box, Button, IconButton } from '@mui/material';
+import { Box, Button, FormControl, IconButton, InputLabel, MenuItem, Select, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
 import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
 import { useNavigate } from "react-router-dom";
 import React from 'react'
 import theme from '../../../assets/theme';
-import { mockExam } from '../../services/APIs/mockData/MockData';
+import { mockBusyLabs, mockExam } from '../../services/APIs/mockData/MockData';
 import { ArrowBack } from '@mui/icons-material';
 
 export default function ExamLabs() {
   const navigate = useNavigate();
-  const exam = mockExam;
+  var exam = mockExam;
+  mockExam.busy_labs = mockBusyLabs;
+  const proctors = ["Mohamed Elhabibi", "Ahmed Elnaggar"]
 
   const getDurationTime = () => {
     var date = new Date(exam.scheduled_date)
@@ -48,8 +50,12 @@ export default function ExamLabs() {
         >
           {exam.title}
         </Box>
-        <Box display="flex" justifyContent="space-between">
-          <Button>Save Changes</Button>
+        <Box display="flex" justifyContent="space-between" alignItems="inherit" >
+          <Button
+            sx={{
+              border: 1,
+              borderRadius: "15px",
+            }}>Save Changes</Button>
         </Box>
       </Box>
       <Box
@@ -72,6 +78,48 @@ export default function ExamLabs() {
             minute: "2-digit",
           })}
         </Box>
+      </Box>
+      <Box
+        sx={{
+          backgroundColor: theme.palette.background.paper,
+          borderRadius: "15px",
+          marginTop: "50px",
+          mx: "4%",
+          px: "10px",
+        }}>
+        <TableContainer>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell align='center'>Lab</TableCell>
+                <TableCell align='center'>Proctor</TableCell>
+                <TableCell align='center'></TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {exam.busy_labs?.map((value, index) =>
+                <TableRow>
+                  <TableCell align='center'>{value.name}</TableCell>
+                  <TableCell>
+                    <FormControl fullWidth size='small'>
+                      <InputLabel>Select Proctor</InputLabel>
+                      <Select>
+                        {proctors.map((value) =>
+                          <MenuItem>{value}</MenuItem>)}
+                      </Select>
+                    </FormControl>
+                  </TableCell>
+                  <TableCell align='right'>
+                    <Button
+                      sx={{
+                        border: 1,
+                        borderRadius: "15px",
+                      }}>View Students</Button>
+                  </TableCell>
+                </TableRow>)}
+            </TableBody>
+          </Table>
+        </TableContainer>
       </Box>
     </Box>
   )
